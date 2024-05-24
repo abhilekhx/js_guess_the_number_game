ba9af1355c5889dc502e39548f4c8d1f706360b2
abhilekhx <lekh7adhikari@gmail.com>
2024-05-24 18:21:43 -0400

    manipulation of DOM

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('hello');
+// alert('yoo')
+
+// variables
+var b = 'smoothie';
+console.log(b);
+
+var someNum = 45;
+
+
+var age = prompt('What is your age?');
+
+document.getElementById('someText').innerHTML = age;
